6a7066b2600b6554b1a7682f54bbb009bddd1054
kavya-priya123 <kavyapriya.marreddy>
2022-05-17 12:56:55 +0530

    commit

---
+++ src/main/java/com/jobiak/mongodb/SpringbootformsMongodbApplication.java
@@ -0,0 +1,15 @@
+package com.jobiak.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootformsMongodbApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringbootformsMongodbApplication.class, args);
+		
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into emp(ename,salary,deptno) values('KING',5000,10);
+insert into emp(ename,salary,deptno) values('JAMES',4000,10);
+insert into emp(ename,salary,deptno) values('WARD',4500,20);
+insert into emp(ename,salary,deptno) values('SMITH',3500,20);
+insert into emp(ename,salary,deptno) values('JONES',4000,30);
+insert into emp(ename,salary,deptno) values('MARTIN',2500,30);
+insert into emp(ename,salary,deptno) values('BLAKE',2000,30);
